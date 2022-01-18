ca305f5b6ef8b854652b716c1da4b036bcf974fd
Keira Wiechecki <kaw504@nyu.edu>
2022-01-18 16:13:31 -0600

    fixed build bug

--- forgeGenome.R
+++ forgeGenome.R
@@ -1,22 +1,12 @@
 library(BSgenome)
 library(optparse)
+library(rtracklayer)
 
 parser <- OptionParser()
 parser <- add_option(parser,'--fasta',action='store',default="HT.Ref.fasta")
 parser <- add_option(parser,'--dir',action='store',default="HT_KY")
 opts <- parse_args(parser)
 
-ky <- readLines(opts$fasta)
-file.start <- grep('>',ky)
-file.end <- c(file.start[-1]-1,length(ky))
-file.names <- paste0(opts$dir,"/",sub('>','',ky[file.start]),'.fa')
-
-# forgeSeqFiles(sub('>','',ky[file.start]), seqfile_name = opts$fasta, seqs_destdir = opts$dir)
-mapply(
-  function(start,end,name) writeLines(ky[start:end],name),
-  file.start,
-  file.end,
-  file.names
-)
-
+ky <- readDNAStringSet(opts$fasta)
+export.2bit(ky,"HT_KY/HT.Ref.2bit")
 forgeBSgenomeDataPkg("DESCRIPTION")
